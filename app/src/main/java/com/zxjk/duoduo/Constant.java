182039a21054f476543eb344af33f52f346adde1
J <574170873@qq.com>
2019-06-06 22:09:46 +0800

    [Version]1.7 [修改内容]1.7发版，修改build文件 [新增功能]none [Note]none [TODO]none

--- app/src/main/java/com/zxjk/duoduo/Constant.java
+++ app/src/main/java/com/zxjk/duoduo/Constant.java
@@ -19,11 +19,8 @@ public class Constant {
     public static final String CITY_DATA = "china_city_data.json";
     //阿里OSS上传地址
     public static final String OSS_URL = "https://zhongxingjike2.oss-cn-hongkong.aliyuncs.com/upload/";
-    public static final String BASE_URL = "https://192.168.0.111:8085/";
-    //public static final String BASE_URL = "https://192.168.0.115:8085/";
-    //public static final String BASE_URL = "https://duoduoweb.zzgb.net.cn";
-    //public static final String BASE_URL = "https://192.168.0.127:8085/";
-    //public static final String BASE_URL = "https://47.75.115.118:8086/";
+//    public static final String BASE_URL = "https://192.168.0.111:8085/";
+    public static final String BASE_URL = "https://duoduoweb.zzgb.net.cn";
     public static final String APP_CODE = "fb0e95b069f74f29a2f972f9454d7d1a";
     public static boolean isVerifyVerision = false;
 
